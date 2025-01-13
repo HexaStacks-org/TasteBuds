f7d25d8c170137e3cb001746764b023caab53387
JomCasti <jomaricastillo8@gmail.com>
2025-01-13 14:34:15 +0800

    Revised footer component for tastebuds #14

--- footer.php
+++ footer.php
@@ -24,7 +24,7 @@
                     <div class="footer-logo">
                         <img src="shared/assets/image/Logo Combination 1.png" alt="TasteBuds Logo">
                     </div>
-                    <p class="open-sans-text">Follow Us</p>
+                    <p class="open-sans-text fw-bold">Follow Us</p>
                     <div class="social-icons d-flex justify-content-center justify-content-md-start">
                         <img src="shared/assets/image/hexastacks_logo.png" alt="Social Icon 1">
                         <img src="shared/assets/image/hexastacks_logo.png" alt="Social Icon 2">
@@ -32,23 +32,23 @@
                 </div>
 
                 <!-- Food Listings -->
-                <div class="col-12 col-md-3 my-3 footer-column d-flex flex-column align-items-md-end">
+                <div class="col-12 col-md-3 my-5 pt-3 footer-column d-flex flex-column align-items-md-end">
                     <h5>Food Listings</h5>
-                    <a href="#">About Us</a>
-                    <a href="#">Terms and Conditions</a>
+                    <h5>Generator</h5>
+                    <h5>Community</h5>
                 </div>
 
                 <!-- Generator -->
-                <div class="col-12 col-md-3 my-3 footer-column d-flex flex-column align-items-md-end">
-                    <h5>Generator</h5>
+                <div class="col-12 col-md-3 my-5 pt-3 footer-column d-flex flex-column align-items-md-end">
+                    <a href="#">About Us</a>
                     <a href="#">Meet The Team</a>
-                    <a href="#">Community Guidelines</a>
+                    <a href="#">Contact</a>
                 </div>
 
                 <!-- Community -->
-                <div class="col-12 col-md-3 my-3 footer-column d-flex flex-column align-items-md-end">
-                    <h5>Community</h5>
-                    <a href="#">Contact</a>
+                <div class="col-12 col-md-3 my-5 pt-3 footer-column d-flex flex-column align-items-md-end">
+                    <a href="#">Community Guidelines</a>
+                    <a href="#">Terms and Conditions</a>
                     <a href="#">Privacy and Policy</a>
                 </div>
             </div>
