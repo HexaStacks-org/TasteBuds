d9327ff500addfd71f24bbff770f4d47e8b862db
JomCasti <jomaricastillo8@gmail.com>
2025-01-21 01:51:04 +0800

    updated footer

--- shared/components/footer.php
+++ shared/components/footer.php
@@ -4,40 +4,40 @@
             <!-- Logo and Socials -->
             <div class="col-12 col-md-3 mb-4 mb-md-0 text-center text-md-start">
                 <div class="footer-logo">
-                    <img src="shared/assets/image/Logo Combination 1.png" alt="TasteBuds Logo">
+                    <a href="index.php"><img src="shared/assets/image/Logo Combination 1.png" alt="TasteBuds Logo"></a>
                 </div>
                 <p class="open-sans-text fw-bold">Follow Us</p>
                 <div class="social-icons d-flex justify-content-center justify-content-md-start">
-                    <img src="shared/assets/image/hexastacks_logo.png" alt="Social Icon 1">
+                    <a href="https://github.com/HexaStacks-org"><img src="shared/assets/image/hexastacks_logo.png" alt="Social Icon 1"></a>
                     <img src="shared/assets/image/hexastacks_logo.png" alt="Social Icon 2">
                 </div>
             </div>
 
             <!-- Food Listings -->
             <div class="col-12 col-md-3 my-5 pt-3 footer-column d-flex flex-column align-items-md-end">
-                <a href="#">
+                <a href="foodListings.php">
                     <h5>Food Listings</h5>
                 </a>
                 <a href="foodgenerator.php">
                     <h5>Generator</h5>
                 </a>
                 <a href="gallery.php">
-                    <h5>Community</h5>
+                    <h5>Gallery</h5>
                 </a>
             </div>
 
             <!-- Generator -->
             <div class="col-12 col-md-3 my-5 pt-3 footer-column d-flex flex-column align-items-md-end">
-                <a href="#">About Us</a>
-                <a href="#">Meet The Team</a>
-                <a href="#">Contact</a>
+                <a href="aboutUs.php">About Us</a>
+                <a href="aboutUs.php">Meet The Team</a>
+                <a href="mailto:tastebuds2025@gmail.com">Contact</a>
             </div>
 
             <!-- Community -->
             <div class="col-12 col-md-3 my-5 pt-3 footer-column d-flex flex-column align-items-md-end">
-                <a href="#">Community Guidelines</a>
-                <a href="#">Terms and Conditions</a>
-                <a href="#">Privacy and Policy</a>
+                <a href="tastebudsOrg.php">Community Guidelines</a>
+                <a href="tastebudsOrg.php">Terms and Conditions</a>
+                <a href="tastebudsOrg.php">Privacy and Policy</a>
             </div>
         </div>
     </div>
