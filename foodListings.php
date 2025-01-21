c95e7a28ac609551b517f327cbb62534baa9b3ea
Kaye Gamana <kayecgamana@gmail.com>
2025-01-21 16:24:55 +0800

    food listings integration

--- foodListings.php
+++ foodListings.php
@@ -1,43 +1,73 @@
 <?php
-include("connect.php");
+include("shared/components/connect.php");
 
-// Initial query setup
-$queryRecipes = "SELECT recipes.*, 
+$primaryCategoryFilter = isset($_GET['primaryCategoryName']) ? $_GET['primaryCategoryName'] : '';
+$subcategoryFilter = isset($_GET['subcategoryName']) ? $_GET['subcategoryName'] : '';
+
+// Query for Recipe Details
+$queryRecipes = "
+    SELECT recipes.*, 
            users.*, 
            images.*, 
-           primaryfoodcategories.primaryCategoryName,
-           foodSubcategories.subcategoryName, 
+           primaryfoodcategories.*,
+           foodSubcategories.*, 
            (SELECT COUNT(likeID) FROM likes WHERE likes.recipeID = recipes.recipeID) AS likesCount,
-           (SELECT COUNT(bookmarkID) FROM bookmarks WHERE bookmarks.recipeID = recipes.recipeID) AS bookmarksCount
+          (SELECT COUNT(bookmarkID) FROM bookmarks WHERE bookmarks.recipeID = recipes.recipeID) AS bookmarksCount
     FROM recipes
     LEFT JOIN users ON users.userID = recipes.userID
     LEFT JOIN images ON images.recipeID = recipes.recipeID
     LEFT JOIN primaryfoodcategories ON primaryfoodcategories.primaryCategoryID = recipes.primaryCategoryID
-    LEFT JOIN foodSubcategories ON foodSubcategories.subcategoryID = recipes.subcategoryID";
+    LEFT JOIN foodSubcategories ON foodSubcategories.subcategoryID = recipes.subcategoryID
+";
 
-// Filters
-$primaryCategoryFilter = isset($_GET['primaryCategoryName']) ? $_GET['primaryCategoryName'] : '';
-$subcategoryFilter = isset($_GET['subcategoryName']) ? $_GET['subcategoryName'] : '';
+// Query for counting the total number of filtered recipes
+$queryCount = "
+    SELECT COUNT(*) AS totalCount
+    FROM recipes
+    LEFT JOIN users ON users.userID = recipes.userID
+    LEFT JOIN images ON images.recipeID = recipes.recipeID
+    LEFT JOIN primaryfoodcategories ON primaryfoodcategories.primaryCategoryID = recipes.primaryCategoryID
+    LEFT JOIN foodSubcategories ON foodSubcategories.subcategoryID = recipes.subcategoryID
+";
 
+// Queries for Filter  
 if ($primaryCategoryFilter != '' || $subcategoryFilter != '') {
-  $queryRecipes .= " WHERE";
+  $queryRecipes = $queryRecipes . " WHERE";
+  $queryCount = $queryCount . " WHERE";
+
 
   if ($primaryCategoryFilter != '') {
-    $queryRecipes .= " primaryfoodcategories.primaryCategoryName='$primaryCategoryFilter'";
+    $queryRecipes = $queryRecipes . " primaryCategoryName='$primaryCategoryFilter'";
+    $queryCount = $queryCount . " primaryCategoryName='$primaryCategoryFilter'";
+
   }
 
   if ($primaryCategoryFilter != '' && $subcategoryFilter != '') {
-    $queryRecipes .= " AND";
+    $queryRecipes = $queryRecipes . " AND";
+    $queryCount = $queryCount . " AND";
+
   }
 
   if ($subcategoryFilter != '') {
-    $queryRecipes .= " foodSubcategories.subcategoryName='$subcategoryFilter'";
+    $queryRecipes = $queryRecipes . " subcategoryName='$subcategoryFilter'";
+    $queryCount = $queryCount . " subcategoryName='$subcategoryFilter'";
+
   }
 }
 
-$resultRecipes = mysqli_query($conn, $queryRecipes);
+$resultRecipes = executeQuery($queryRecipes);
+$countResult = executeQuery($queryCount);
+
+$primaryCategoryQuery = "SELECT DISTINCT(primaryCategoryName) FROM primaryFoodCategories";
+$primaryCategoryResults = executeQuery($primaryCategoryQuery);
+
+$subcategoryQuery = "SELECT DISTINCT(subcategoryName) FROM foodSubcategories";
+$subcategoryResults = executeQuery($subcategoryQuery);
 
 ?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -45,11 +75,8 @@ $resultRecipes = mysqli_query($conn, $queryRecipes);
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Food Listings</title>
-  <link
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-    rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-    crossorigin="anonymous" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
   <link rel="stylesheet" href="shared/assets/css/foodListingsRecipe.css" />
   <link rel="stylesheet" href="shared/assets/css/style.css">
 </head>
@@ -92,19 +119,35 @@ $resultRecipes = mysqli_query($conn, $queryRecipes);
   <!-- Recipe Display -->
   <div class="container-fluid mt-5 content-bg">
     <div class="row">
+
+
+      <!--  The total count for results-->
+      <div class="row mt-5 mb-3">
+        <div class="col text-center" style="color:white;">
+          <?php
+          if (mysqli_num_rows($countResult) > 0) {
+            while ($countResultRow = mysqli_fetch_assoc($countResult)) {
+              ?>
+              <h4>TOTAL RESULTS: <?php echo $countResultRow['totalCount'] ?></h4>
+              <?php
+            }
+          } ?>
+        </div>
+      </div>
+
       <?php if (mysqli_num_rows($resultRecipes) > 0) {
-        while ($userRow = mysqli_fetch_assoc($resultRecipes)) { ?>
+        while ($recipesRow = mysqli_fetch_assoc($resultRecipes)) { ?>
           <div class="col-md-6 pt-5">
             <div class="card">
               <div class="row g-0">
                 <div class="col-md-4">
-                  <img src="shared/assets/image/test-pic.png" class="img-fluid">
+                  <img src="shared/assets/image/content-image/<?php echo $recipesRow['imageURL']; ?>" class="img-fluid">
                 </div>
                 <div class="col-md-8">
                   <div class="card-body">
-                    <span class="category"><?php echo $userRow["primaryCategoryName"]; ?></span>
-                    <h5 class="card-title"><?php echo $userRow["recipeTitle"]; ?></h5>
-                    <p class="card-text"><?php echo $userRow["description"]; ?></p>
+                    <span class="category"><?php echo $recipesRow["primaryCategoryName"]; ?></span>
+                    <h5 class="card-title"><?php echo $recipesRow["recipeTitle"]; ?></h5>
+                    <p class="card-text"><?php echo $recipesRow["description"]; ?></p>
                   </div>
                 </div>
               </div>
@@ -112,7 +155,13 @@ $resultRecipes = mysqli_query($conn, $queryRecipes);
           </div>
         <?php }
       } else { ?>
-        <p class="text-center">No recipes found.</p>
+        <div class="container">
+          <div class="row">
+            <div class="col d-flex justify-content-center" style="width: 100%;">
+              <img src="shared/assets/image/no-results-found.png" style="max-width: 50%; height: auto; margin: 20px 0 40px 0" class="img-fluid">
+            </div>
+          </div>
+        </div>
       <?php } ?>
     </div>
   </div>
@@ -141,7 +190,7 @@ $resultRecipes = mysqli_query($conn, $queryRecipes);
     }
 
     // Automatically Manage Button Visibility on Page Load
-    window.onload = function() {
+    window.onload = function () {
       const url = new URL(window.location.href);
       const primaryCategory = url.searchParams.get('primaryCategoryName');
       const subcategory = url.searchParams.get('subcategoryName');
@@ -155,8 +204,7 @@ $resultRecipes = mysqli_query($conn, $queryRecipes);
       }
     };
   </script>
-  <script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
     crossorigin="anonymous"></script>
 </body>
