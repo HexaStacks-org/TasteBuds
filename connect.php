38cdaee33ca107ee3d0f21d09437eace853703bc
unknown <njvergara12@gmail.com>
2025-01-22 01:35:05 +0800

    generator

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
-$db = "trytastebuds";
+$db = "trytastebuds2";
 
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
 
