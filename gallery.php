892369375ab0fbead8eb14d6bd31de21ebfc0d7a
davidgortizjr <nyxdesu3@gmail.com>
2025-01-21 14:01:38 +0800

    added functionality for the report button

--- gallery.php
+++ gallery.php
@@ -31,6 +31,55 @@
         </div>
     </div>
 
+    <div class="modal fade" id="reportModal" tabindex="-1" aria-labelledby="reportModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="reportModalLabel">Report Content</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form id="reportForm">
+                        <p>Please select the reason for reporting this content:</p>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="reportReason" id="spam" value="Spam"
+                                required>
+                            <label class="form-check-label" for="spam">Spam</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="reportReason" id="wrongInfo"
+                                value="Wrong Information">
+                            <label class="form-check-label" for="wrongInfo">Wrong Information</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="reportReason" id="inappropriateContent"
+                                value="Inappropriate Content">
+                            <label class="form-check-label" for="inappropriateContent">Inappropriate Content</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="reportReason" id="copyrightInfringement"
+                                value="Copyright Infringement">
+                            <label class="form-check-label" for="copyrightInfringement">Copyright Infringement</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="reportReason" id="harassmentAbuse"
+                                value="Harassment or Abuse">
+                            <label class="form-check-label" for="harassmentAbuse">Harassment or Abuse</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="reportReason" id="other" value="Other">
+                            <label class="form-check-label" for="other">Other</label>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-primary" id="submitReport">Submit Report</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <script src="shared/assets/js/gallery.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
--- shared/assets/js/gallery.js
+++ shared/assets/js/gallery.js
@@ -155,4 +155,26 @@ nextBtn.addEventListener('click', function () {
     }
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    var reportButtons = document.querySelectorAll('.btn-report');
+
+    for (var i = 0; i < reportButtons.length; i++) {
+        reportButtons[i].addEventListener('click', function () {
+            var reportModal = new bootstrap.Modal(document.getElementById('reportModal'));
+            reportModal.show();
+        });
+    }
+
+    document.getElementById('submitReport').addEventListener('click', function () {
+        var selectedReason = document.querySelector('input[name="reportReason"]:checked');
+        if (selectedReason) {
+            alert('Report submitted for reason: ' + selectedReason.value);
+            var reportModal = bootstrap.Modal.getInstance(document.getElementById('reportModal'));
+            reportModal.hide();
+        } else {
+            alert('Please select a reason for reporting.');
+        }
+    });
+});
+
 renderCards();
