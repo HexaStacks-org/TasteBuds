79bf56befa52f9d944afe252067d413a64d1c735
Anyl Feboe Uriarte <uriarteanylfeboe@gmail.com>
2025-01-22 01:04:39 +0800

    minor changes, fixed file directory

--- admin/management.php
+++ admin/management.php
@@ -8,9 +8,9 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
   <?php
-  include("shared\components\fontEmbed.php");
+  include("shared/components/fontEmbed.php");
   ?>
-  <link rel="stylesheet" href="shared\assets\css\admin.css" />
+  <link rel="stylesheet" href="shared/assets/css/admin.css" />
 </head>
 
 <body>
@@ -19,13 +19,13 @@
     <div class="row">
       <!-- sidebar-->
       <?php
-      include("shared\components\sidebar.php");
+      include("shared/components/sidebar.php");
       ?>
 
       <div class="col-md-8 col-lg-10 p-0">
         <!-- offcanvas menu -->
         <?php
-        include("shared\components\offcanvas.php");
+        include("shared/components/offcanvas.php");
         ?>
 
         <!-- content -->
