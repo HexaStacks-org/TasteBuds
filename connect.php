89ac77aec9c9a1caef6b261d3721df4ab0651b4c
unknown <njvergara12@gmail.com>
2025-01-22 20:44:13 +0800

    update connect

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
-$db = "trytastebuds2";
+$db = "tastebuds";
 
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
 
