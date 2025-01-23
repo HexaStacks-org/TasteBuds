22d5800dbc20f62d9303add056e032dd26481ad4
davidgortizjr <nyxdesu3@gmail.com>
2025-01-24 05:22:07 +0800

    fixed the fab button

--- users/index.php
+++ users/index.php
@@ -135,8 +135,7 @@ $resultTopRecipes = executeQuery($queryTopRecipes);
         </div>
     </section>
 
-    <?php include '../shared/components/footer.php'; ?> 
-    <?php include '../shared/components/createContent.php'; ?> 
+    <?php include '../shared/components/footer.php'; ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
