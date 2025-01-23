e0fe5bb404fbde0177e9c016464d55788b6c3dc5
davidgortizjr <nyxdesu3@gmail.com>
2025-01-24 04:41:35 +0800

    fixed href of footer

--- shared/components/footer.php
+++ shared/components/footer.php
@@ -4,11 +4,13 @@
             <!-- Logo and Socials -->
             <div class="col-12 col-md-3 mb-4 mb-md-0 text-center text-md-start">
                 <div class="footer-logo">
-                    <a href="index.php"><img src="../shared/assets/image/Logo Combination 1.png" alt="TasteBuds Logo"></a>
+                    <a href="index.php"><img src="../shared/assets/image/Logo Combination 1.png"
+                            alt="TasteBuds Logo"></a>
                 </div>
                 <p class="open-sans-text fw-bold">Follow Us</p>
                 <div class="social-icons d-flex justify-content-center justify-content-md-start">
-                    <a href="https://github.com/HexaStacks-org"><img src="../shared/assets/image/hexastacks_logo.png" alt="Social Icon 1"></a>
+                    <a href="https://github.com/HexaStacks-org"><img src="../shared/assets/image/hexastacks_logo.png"
+                            alt="Social Icon 1"></a>
                     <img src="../shared/assets/image/hexastacks_logo.png" alt="Social Icon 2">
                 </div>
             </div>
@@ -35,9 +37,9 @@
 
             <!-- Community -->
             <div class="col-12 col-md-3 my-5 pt-3 footer-column d-flex flex-column align-items-md-end">
-                <a href="tastebudsOrg.php">Community Guidelines</a>
+                <a href="#community-guidelines">Community Guidelines</a>
                 <a href="tastebudsOrg.php">Terms and Conditions</a>
-                <a href="tastebudsOrg.php">Privacy and Policy</a>
+                <a href="#privacy-policy">Privacy and Policy</a>
             </div>
         </div>
     </div>
