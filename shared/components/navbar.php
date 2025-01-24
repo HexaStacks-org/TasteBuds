bff4bae841bed6ac7642cde6db863eb8c7db4a0c
davidgortizjr <nyxdesu3@gmail.com>
2025-01-24 12:09:14 +0800

    minor changes

--- shared/components/navbar.php
+++ shared/components/navbar.php
@@ -43,11 +43,11 @@
                     <button class="btn user-btn" type="button" id="userButton">
                         <p>User</p>
                     </button>
-                    <button type="button" class="btn dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown"
+                    <button type="button" class="btn dropdown-toggle dropdown-toggle-split " data-bs-toggle="dropdown"
                         aria-expanded="false" aria-label="Toggle Dropdown">
                         <span class="visually-hidden">Toggle Dropdown</span>
                     </button>
-                    <ul class="dropdown-menu dropdown-menu-end user-dropdown-menu">
+                    <ul class="dropdown-menu dropdown-menu-end user-dropdown-menu ">
                         <li><a class="dropdown-item" href="updateUser.php">Edit Profile</a></li>
                         <li><a class="dropdown-item" href="userContent.php">Uploaded Contents</a></li>
                         <li><a class="dropdown-item" href="userLike.php">Likes</a></li>
