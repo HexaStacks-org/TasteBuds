ef421b901ddfc8637ad452f569bd63bccb6dce75
davidgortizjr <nyxdesu3@gmail.com>
2025-01-24 18:40:47 +0800

    session injection

--- admin/shared/process/session.php
+++ admin/shared/process/session.php
@@ -1,10 +1,12 @@
 <?php
-  session_start();
-  
-  $role = $_SESSION['role'];
-  
-  if (isset($_SESSION['role']) || $_SESSION[$role == 'user']) {
-      header("Location: ../landing.php");
-      exit();
-  }
+include("../connect.php");
+session_start();
+
+if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
+    header("Location: ../login.php");
+}
+
+if (isset($_GET['role'])) {
+    $requestedUserRole = $_GET['role'];
+}
 ?>
